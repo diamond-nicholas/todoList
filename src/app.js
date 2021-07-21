b6d4cb360f234763b7f4ddb959fc6bd4d1cb2b2d
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-07-21 07:55:38 +0100

    add the first structure of the projects and task with font awesome add icons and cdn

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// const myBtn = () => {
+//   const content = document.querySelector("#content");
+//   const btn = document.createElement("button");
+//   btn.textContent = "this is working";
+//   content.appendChild(btn);
+// };
+
+// export default myBtn;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-import "./styles.css";
+// import myBtn from "./app";
+// import "./styles.css";
+
+// document.body.appendChild(myBtn());
