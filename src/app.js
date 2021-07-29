9e5189ab5724660df4fe9fbcdcc9330c7eb52583
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-07-29 04:04:14 +0100

    complete project append section

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-const listsContainer = document.querySelector(".test");
+const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listDisplayContainer = document.querySelector(
+  "[data-list-display-container]"
+);
+const listTitleElement = document.querySelector("[data-list-title]");
+const listCountElement = document.querySelector("[data-list-count]");
+const taskContainer = document.querySelector("[data-tasks]");
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+console.log(listsContainer);
 
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [
-  {
-    id: 1,
-    name: "Default",
-  },
-];
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 const clearElement = (element) => {
   while (element.firstChild) {
@@ -18,17 +23,30 @@ const clearElement = (element) => {
 
 const render = () => {
   clearElement(listsContainer);
+  renderList();
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = "none";
+  } else {
+    listDisplayContainer.style.display = "";
+  }
+};
+
+const renderList = () => {
   lists.forEach((list) => {
     const listElement = document.createElement("li");
     listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
     listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list");
+    }
     listsContainer.appendChild(listElement);
   });
 };
 
 const saveToLocal = () => {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
 };
 
 const saveAndRender = () => {
@@ -45,8 +63,20 @@ newListForm.addEventListener("submit", (e) => {
   saveAndRender();
 });
 
+listsContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
+deleteListButton.addEventListener("click", (e) => {
+  lists = lists.filter((list) => list.id != selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
+
 const createList = (name) => {
   return { id: Date.now().toString(), name: name, task: [] };
 };
-
 export default render;
